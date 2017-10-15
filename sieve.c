21534a240ef0eb480ab83a20b0e8b3400426818a
gazingatnavel <gazingatnavel@users.noreply.github.com>
2017-10-15 10:09:38 -0400

    Add files via upload

---
+++ sieve.c
@@ -0,0 +1,22 @@
+/* Precondition: a is an array of length n.
+   Postcondition: elements of a at prime indices are 1, all other elements 
+                  are 0.
+*/
+void sieve (int a[], int n) {
+    int i, j;
+    /* Initialize all elements, except first two, to 1. */
+    a[0] = a[1] = 0;
+    for (i = 2; i < n; i += 1) {
+        a[i] = 1;
+    }
+    /* Iterate array, marking each element that is at a composite (not prime)
+       index to 0. */
+    for (i = 2; i < n; i += 1) {
+        if (a[i] == 1) {
+            for (j = i + i; j < n; j += i) {
+                a[j] = 0;
+            }
+        }
+    }
+}
+    
